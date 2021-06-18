7b708c21afc5dfbf3fed4ffd48bd39574f2c06e8
vvvictorlee <lisheng.ipad@icloud.com>
2021-06-19 06:52:42 +0800

    add net

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -4,7 +4,7 @@ import rinkeby from './rinkeby'
 import xdai from './xdai'
 import energy_web_chain from './energy_web_chain'
 import volta from './volta'
-
+import hsctest from './hsctest'
 export default {
   local,
   mainnet,
@@ -12,4 +12,5 @@ export default {
   xdai,
   energy_web_chain,
   volta,
+  hsctest
 }
