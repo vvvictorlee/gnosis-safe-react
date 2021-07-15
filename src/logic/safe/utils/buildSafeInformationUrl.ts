e9562ead5819d0c0e96178e9ab4c1f9524469192
vvvictorlee <lisheng.ipad@icloud.com>
2021-07-15 10:31:11 +0800

    trusted

--- src/logic/safe/utils/buildSafeInformationUrl.ts
+++ src/logic/safe/utils/buildSafeInformationUrl.ts
@@ -4,5 +4,5 @@ import { checksumAddress } from 'src/utils/checksumAddress'
 export const buildSafeInformationUrl = (safeAddress: string): string => {
   const address = checksumAddress(safeAddress)
   const url = getSafeServiceBaseUrl(address)
-  return `${url}/`
+  return `${url}/?trusted=true`
 }
